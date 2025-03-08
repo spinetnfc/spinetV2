b89195679dbc861a664d682bb8617499cd8f6183
Imad-t <imadeddineterraf@gmail.com>
2025-03-08 11:19:39 +0100

    fix shadcn table

--- src/components/ui/table/pagination.tsx
+++ src/components/ui/table/pagination.tsx
@@ -5,7 +5,7 @@ import {
 } from '@radix-ui/react-icons';
 import * as React from 'react';
 
-import { ButtonProps, buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/utils/cn';
 
 import { Link } from '../link';
@@ -42,13 +42,11 @@ PaginationItem.displayName = 'PaginationItem';
 
 type PaginationLinkProps = {
   isActive?: boolean;
-} & Pick<ButtonProps, 'size'> &
-  React.ComponentProps<'a'>;
+} & React.ComponentProps<'a'>;
 
 const PaginationLink = ({
   className,
   isActive,
-  size = 'icon',
   children,
   href,
   ...props
@@ -59,7 +57,6 @@ const PaginationLink = ({
     className={cn(
       buttonVariants({
         variant: isActive ? 'outline' : 'ghost',
-        size,
       }),
       className,
     )}
@@ -76,7 +73,6 @@ const PaginationPrevious = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to previous page"
-    size="default"
     className={cn('gap-1 px-2.5', className)}
     {...props}
   >
@@ -92,7 +88,6 @@ const PaginationNext = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to next page"
-    size="default"
     className={cn('gap-1 pe-2.5', className)}
     {...props}
   >
