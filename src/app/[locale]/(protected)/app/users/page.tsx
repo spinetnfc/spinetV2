51584a6e51c547414efac04f7fcc6e87c5b3108e
Imad-t <imadeddineterraf@gmail.com>
2025-04-22 16:00:13 +0100

    remove extra imports from users page in app route

--- src/app/[locale]/(protected)/app/users/page.tsx
+++ src/app/[locale]/(protected)/app/users/page.tsx
@@ -1,17 +1,10 @@
 
 import { ContentLayout } from '@/components/layouts/content-layout';
-import { UsersList } from '@/features/users/components/users-list';
-import { Authorization, ROLES } from '@/lib/authorization';
 
 const UsersPage = () => {
   return (
     <ContentLayout title="Users">
-      <Authorization
-        forbiddenFallback={<div>Only admin can view this.</div>}
-        allowedRoles={[ROLES.ADMIN]}
-      >
-        <UsersList />
-      </Authorization>
+      <></>
     </ContentLayout>
   );
 };
