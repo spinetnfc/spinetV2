78ad5833815c248e6cc52fbbf166b6618221c808
Imad-t <imadeddineterraf@gmail.com>
2025-05-14 14:23:31 +0100

    add update and delete contact routes

--- src/lib/api/contacts.ts
+++ src/lib/api/contacts.ts
@@ -35,30 +35,38 @@ export const addContact = async (profileId: string, contact: ContactInput): Prom
     }
 }
 
-// export const updateService = async (profileId: string, serviceId: string, service: ServiceInput): Promise<{ message: string }> => {
-//     if (!profileId || typeof profileId !== 'string') {
-//         throw new Error(`Invalid profileId: ${profileId}`);
-//     }
-//     if (!serviceId || typeof serviceId !== 'string') {
-//         throw new Error(`Invalid serviceId: ${serviceId}`);
-//     }
+export const updateContact = async (profileId: string, contact: ContactInput): Promise<{ message: string }> => {
+    const headers = await withServerCookies();
+    try {
+        if (!profileId || typeof profileId !== 'string') {
+            throw new Error(`Invalid profileId: ${profileId}`);
+        }
+        console.log("contact:::::::::::::::::::", JSON.stringify(contact, null, 2));
+        const response = await ServerApi.patch(`/profile/${profileId}/contacts`, contact, { headers });
+        console.log("Contact added response received:", response.status);
 
-//     const response = await api.patch(`/profile/${profileId}/service/${serviceId}`, service);
-//     console.log("Service updated response received:", response.status);
-
-//     return response.data;
-// }
+        return response.data;
+    } catch (error) {
+        console.error('Error adding contact:', error);
+        throw error;
+    }
+}
 
 export const deleteService = async (profileId: string, contactId: string): Promise<{ message: string }> => {
-    if (!profileId || typeof profileId !== 'string') {
-        throw new Error(`Invalid profileId: ${profileId}`);
-    }
-    if (!contactId || typeof contactId !== 'string') {
-        throw new Error(`Invalid contactId: ${contactId}`);
-    }
+    const headers = await withServerCookies();
+    try {
+        if (!profileId || typeof profileId !== 'string') {
+            throw new Error(`Invalid profileId: ${profileId}`);
+        }
+        if (!contactId || typeof contactId !== 'string') {
+            throw new Error(`Invalid contactId: ${contactId}`);
+        }
+        const response = await ServerApi.delete(`/profile/${profileId}/contact/${contactId}`, { headers });
+        console.log("Service deleted response received:", response.status);
 
-    const response = await ServerApi.delete(`/profile/${profileId}/contact/${contactId}`);
-    console.log("Service deleted response received:", response.status);
-
-    return response.data;
+        return response.data;
+    } catch (error) {
+        console.error('Error adding contact:', error);
+        throw error;
+    }
 }
