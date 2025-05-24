519dc79b54b907070a68238580f1252692b8dacc
Imad-t <imadeddineterraf@gmail.com>
2025-05-24 16:26:05 +0100

    import user type from /types instead of authcontext

--- src/utils/server-cookie.ts
+++ src/utils/server-cookie.ts
@@ -1,15 +1,20 @@
-import { User } from '@/context/authContext';
+import type { User } from '@/types/user';
 import { cookies } from 'next/headers';  // for server-side cookie handling
 
 export async function getUserCookieOnServer(): Promise<User | null> {
   const cookieStore = await cookies();
+  const sessionId = cookieStore.get('spinet-session')?.value;
   const userCookie = cookieStore.get('current-user')?.value;
 
-  if (userCookie) {
-    const userFromCookie = JSON.parse(decodeURIComponent(userCookie));
-    return userFromCookie;
-  } else {
-    console.error('No user cookie found');
+  if (!sessionId || !userCookie) {
+    return null;
+  }
+
+  try {
+    const user: User = JSON.parse(decodeURIComponent(userCookie));
+    return user;
+  } catch (error) {
+    console.error('Error parsing user cookie:', error);
     return null;
   }
 }
