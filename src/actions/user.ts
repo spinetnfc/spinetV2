b6e8720b2c5401ff2a3960a3ea3b8e7d959b5bf2
Imad-t <imadeddineterraf@gmail.com>
2025-06-16 14:59:06 +0100

    update updated user data type in the server action

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -2,7 +2,7 @@
 import { updateUser } from "@/lib/api/user";
 import { User } from "@/types/user";
 
-export async function updateUserAction(userId: string, user: User) {
+export async function updateUserAction(userId: string, user: Partial<User>) {
     try {
         await updateUser(userId, user);
         return { success: true };
