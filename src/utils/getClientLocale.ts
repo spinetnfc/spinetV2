f3ee79e2ab721406e4eeec9832a402b259f3d542
Imad-t <imadeddineterraf@gmail.com>
2025-06-16 15:38:35 +0100

    use useParams to get locale on client always

--- src/utils/getClientLocale.ts
+++ src/utils/getClientLocale.ts
@@ -1,13 +1,7 @@
-export function getLocale(): string | undefined {
-    const pathname = typeof window !== "undefined" ? window.location.pathname : "";
-    const pathLocale = pathname.split("/")[1]; // first segment after /
+import { useParams } from "next/navigation";
 
-    if (pathLocale && /^[a-z]{2}(-[A-Z]{2})?$/.test(pathLocale)) {
-        return pathLocale;
-    }
-    if (typeof document === 'undefined') return undefined;
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; NEXT_LOCALE=`);
-    if (parts.length === 2) return parts.pop()?.split(';').shift();
-    return "en";
+export function getLocale(): string | undefined {
+    const { locale } = useParams();
+    const currentLocale = typeof locale === 'string' ? locale : 'en';
+    return currentLocale;
 }
