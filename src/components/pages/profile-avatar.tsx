fcaeaf83d6289f8ac1d907e1ab56e39f159afa3a
Imad-t <imadeddineterraf@gmail.com>
2025-07-02 13:12:06 +0100

    add profile picture to use menu

--- src/components/pages/profile-avatar.tsx
+++ src/components/pages/profile-avatar.tsx
@@ -30,7 +30,7 @@ export function ProfileAvatar({ profilePicture, alt }: { profilePicture?: string
             alt={alt}
             width={40}
             height={40}
-            className="w-10 h-10 rounded-full object-cover"
+            className="w-10 h-10 rounded-full object-cover border border-white"
             unoptimized
         />
     );
--- src/components/userMenu.tsx
+++ src/components/userMenu.tsx
@@ -11,13 +11,34 @@ import { FormattedMessage } from "react-intl";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/authContext";
 import SwitchProfile from "@/components/switch-profile-drawer";
+import { useEffect, useState } from "react";
+import { getProfileAction } from "@/actions/profile";
+import { getFile } from "@/actions/files";
+import Image from "next/image";
 const UserMenu = ({ locale }: { locale: string }) => {
-    const router = useRouter();
-    const { logout } = useAuth();
+    const { logout, user } = useAuth();
+    const profileId = user?.selectedProfile || null;
+    const [imageUrl, setImageUrl] = useState<string | null>(null);
+    useEffect(() => {
+        let isMounted = true;
+        async function fetchImage() {
+            const profile = await getProfileAction(profileId);
+            if (profile.profilePicture) {
+                try {
+                    const url = await getFile(profile.profilePicture);
+                    if (isMounted) setImageUrl(url);
+                } catch {
+                    if (isMounted) console.log("coundlen't fetch profile image");
+
+                }
+            }
+        }
+        fetchImage();
+    }, [profileId]);
     return (<DropdownMenu>
         <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon" className="rounded-full bg-white dark:bg-background">
-                <User2 className="size-6" />
+            <Button variant="outline" size="icon" className="rounded-full bg-white dark:bg-background p-0">
+                {imageUrl ? <Image src={imageUrl} alt="Profile Picture" width={32} height={32} className="rounded-full object-cover" /> : <User2 className="size-6" />}
                 <span className="sr-only">Open user menu</span>
             </Button>
         </DropdownMenuTrigger>
